c2e2c9f91013a2eb89a5f80cd26f32c4731aaa0e
Ryo Kawamura <r.kawamura.syukatu@gmail.com>
2018-11-03 03:07:50 +0000

    first commit

---
+++ tweet_app/db/.backup.sql
@@ -0,0 +1,16 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2017-03-31 05:21:00.324397','2017-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'次は何の本を読もうかな。','2017-03-31 05:24:15.529063','2017-03-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'今日のランチおいしかった。','2017-03-31 05:24:32.004378','2017-03-31 05:24:32.004378');
+INSERT INTO posts VALUES(3,'Rails勉強中！','2018-10-29 13:48:59.773453','2018-10-29 13:48:59.773453');
+INSERT INTO posts VALUES(4,'yeah!!!','2018-10-31 14:31:44.000504','2018-10-31 14:31:44.000504');
+INSERT INTO posts VALUES(5,'yeah!!!','2018-10-31 14:31:46.134805','2018-10-31 14:31:46.134805');
+INSERT INTO posts VALUES(6,'ねむい！','2018-10-31 14:32:00.938729','2018-10-31 14:32:00.938729');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',6);
+COMMIT;
